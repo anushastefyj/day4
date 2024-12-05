29b1a65fff7f3a9fb08dfbc3706b44622cc46851
anushastefyj <24hr5a3001@mtieat.org>
2024-12-05 12:32:59 +0530

    Add files via upload

---
+++ Forodd.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Forodd
+{
+public static void main(String args[])
+{
+    Scanner Sc=new Scanner(System.in);
+    int a=Sc.nextInt();
+    int b=Sc.nextInt();
+for(int i=1;i<=a;i+=2)
+{
+    System.out.println("anu");
+}
+for(int j=1;j<=b;j+=2)
+{
+    System.out.println("stefy");
+}
+}
+}
